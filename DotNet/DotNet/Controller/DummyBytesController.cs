4e3076b125142d5e75cb38fa68a5e1bfb1bdfecb
sasan <sss@gmail.com>
2019-11-08 03:14:29 +0330

    Add DotNet Implementation

---
+++ DotNet/DotNet/Controller/DummyBytesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace DotNet.Controller
+{
+    public class DummyBytesController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Get()
+        {
+            HttpResponseMessage Response;
+
+            string UserAgent = HttpContext.Current.Request.UserAgent;
+            if(UserAgent.Contains("Instagram"))
+            {
+                Response = new HttpResponseMessage(HttpStatusCode.OK);
+                Response.Content = new ByteArrayContent(new byte[50]);
+                Response.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                return Response;
+            }
+            else
+            {
+                Response = Request.CreateResponse(HttpStatusCode.Redirect);
+                Response.Headers.Location = new Uri("https://mywebsite.com");
+                return Response;
+            }
+        }
+    }
+}
---
+++ DotNet/DotNet/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using DotNet.Configuration;
+using System;
+using System.Web;
+using System.Web.Http;
+
+namespace DotNet
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
